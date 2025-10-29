73bb524d5a50c02cc49ae5b08886a04e3b815621
PyAuto-ADMINISTRATOR <pyautoadm@gmail.com>
2025-10-28 23:11:30 -0400

    Delete src directory

--- src/32-Bit-Token-Hex/32-Bit-Token-Hex.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-import secrets
-print(secrets.token_hex(32))
--- src/Hash-Passwd/Hash-Passwd.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import bcrypt
-a = input('')
-print(str(bcrypt.hashpw(a.encode(), bcrypt.gensalt()).decode()))
